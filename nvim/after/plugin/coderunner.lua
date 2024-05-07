426d6c493f6aeb88832cb2b226ae339be50ddc3d
Jagannath Hari <jagannathhari17@gmail.com>
2024-05-07 10:59:30 +0530

    change execuatble path and added langague in coderunner.lua

--- nvim/after/plugin/coderunner.lua
+++ nvim/after/plugin/coderunner.lua
@@ -1,20 +1,20 @@
 require('code_runner').setup({
-  mode= 'term',
-  focus = true,
-  startinsert = true,
-   term = {
-		position = "bot",
-		size = 8,
-	},
-  filetype = {
-		java = "cd $dir &&  /home/default/Applications/jdk-19.0.1/bin/javac $fileName && /home/default/Applications/jdk-19.0.1/bin/java $fileNameWithoutExt",
-		--python = "/home/default/.pyenv/shims/python -u",
-		python = "python3 -u",
-		typescript = "deno run",
-		rust = "cd $dir && rustc -o $fileNameWithoutExt $fileName && $dir/$fileNameWithoutExt",
-        c = "cd $dir && gcc -o $fileNameWithoutExt $fileName -O3 -lm && $dir/$fileNameWithoutExt && rm $dir/$fileNameWithoutExt",
+    mode= 'term',
+    focus = true,
+    startinsert = true,
+    term = {
+    position = "bot",
+    size = 8,
+    },
+    filetype = {
+    java = "cd $dir &&  /home/default/Applications/jdk-19.0.1/bin/javac $fileName && /home/default/Applications/jdk-19.0.1/bin/java $fileNameWithoutExt",
+        python = "/home/user/.pyenv/shims/python -u",
+        go = "cd $dir && go run $fileName",
+        typescript = "deno run",
+        rust = "cd $dir && rustc -o $fileNameWithoutExt $fileName && $dir/$fileNameWithoutExt",
+        c = "cd $dir && gcc -o $fileNameWithoutExt $fileName -O0 -lm && $dir/$fileNameWithoutExt && rm $dir/$fileNameWithoutExt",
         lua = "lua",
         kivy = "/home/jagannath/.pyenv/shims/python main.py",
 
-	},
+    },
 })
