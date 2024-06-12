3bb62c223e4a9ddb234a9c1be5984cbbd3fd0040
Jagannath Hari <jagannathhari17@gmail.com>
2024-06-13 02:38:44 +0530

    changed telescope layout

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,8 +1,36 @@
+
+local actions = require "telescope.actions"
+require("telescope").setup {
+    defaults = {
+      sorting_strategy = "ascending",
+      layout_strategy = "bottom_pane",
+      results_title = "",
+      file_ignore_patterns = { "node_modules",".pyc" },
+      borderchars = { " ", " ", " ", " ", " ", " ", " ", " " },
+      prompt_prefix=":",
+      preview=false,
+      layout_config = {
+         prompt_position = "bottom",
+             height = 10,
+      },
+},
+  pickers = {
+    buffers = {
+      mappings = {
+        n = {
+          ["<c-d>"] = actions.delete_buffer ,
+        }
+      }
+    },
+
+  }
+}
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>of', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ob', builtin.buffers, {})
+vim.keymap.set('n', '<leader>jf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>jb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>jg', builtin.live_grep, {})
